89ca463859c3439c493776a79b011b053d98dab8
Nadav Menirav <nmenirav@gmail.com>
2025-11-23 19:08:03 +0200

    (Feat)

--- ex1.py
+++ ex1.py
@@ -11,8 +11,12 @@ ROBOTS = "Robots"
 id = ["No numbers - I'm special!"]
 
 class State:
-    size = 0 # Size of the board. no reason to save it in every copy.
-    walls = 0 # The walls. no reason to save it in every copy.
+    size = (0, 0) # Size of the board. no reason to save it in every copy.
+    walls = frozenset() # The walls. no reason to save it in every copy.
+    taps: dict[tuple, int]
+    plants: dict[tuple, int]
+    robots: dict[int, tuple]
+
 
     def __init__(self, initial):
         State.size = initial.size
@@ -34,8 +38,10 @@ class WateringProblem(search.Problem):
     def successor(self, state):
         """ Generates the successor states returns [(action, achieved_states, ...)]"""
 
-    def goal_test(self, state):
+    def goal_test(self, state: State) -> bool:
         """ given a state, checks if this is the goal state, compares to the created goal state returns True/False"""
+
+
 
     def h_astar(self, node):
         """ This is the heuristic. It gets a node (not a state)
