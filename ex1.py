8a84448a745c331c174d9c694106a59ce5dc9340
Nadav Menirav <nmenirav@gmail.com>
2025-11-28 13:31:09 +0200

    (Feat) now robots can load water

--- ex1.py
+++ ex1.py
@@ -152,7 +152,6 @@ class WateringProblem(search.Problem):
                     print("ERROR WATER NEEDED IN PLANT IS NEGATIVE")
                     break
 
-
                 # Changing the robot's load
                 new_robot_key_tuple = (x, y)
                 new_robot_value_tuple = (id, load - 1, capacity)
@@ -179,6 +178,47 @@ class WateringProblem(search.Problem):
                 # Inserting the new state to the possible next states
                 possible_successors.append((f"POUR{{{id}}}", new_state))
 
+
+            # We now want to check whether the robot can load more WU from a tap
+            if capacity - load > 0:
+
+                # We now want to check whether the robot is on a tap it can load water from
+                # We don't care whether there is a tap that can give 0 WU or if there isn't a tap at all
+                water_available_in_tap_under_robot = state.robots.get((x, y), 0)
+
+                if water_available_in_tap_under_robot == 0:
+                    break
+
+                # For testing:
+                if water_available_in_tap_under_robot < 0:
+                    print("ERROR WATER AVAILABLE IN TAP IS NEGATIVE")
+                    break
+
+                # Changing the robot's load
+                new_robot_key_tuple = (x, y)
+                new_robot_value_tuple = (id, load + 1, capacity)
+
+                # Changing the tap
+                new_tap_key_tuple = (x, y)
+                new_tap_value = water_available_in_tap_under_robot - 1
+
+                # Creating the new state
+                new_state = State(size = state.size,
+                                  walls = state.walls,
+                                  taps = state.taps,
+                                  plants = state.plants,
+                                  robots = state.robots)
+
+                # Deleting the previous state of robot and inserting the new one
+                del new_state.robots[(x, y)]
+                new_state.robots[new_robot_key_tuple] = new_robot_value_tuple
+
+                # Deleting the previous state of tap and inserting the new one
+                del new_state.taps[(x, y)]
+                new_state.plants[new_tap_key_tuple] = new_tap_value
+
+                # Inserting the new state to the possible next states
+                possible_successors.append((f"LOAD{{{id}}}", new_state))
 
 
         return possible_successors
